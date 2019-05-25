f25f8b48bff179a5da75b6046af657864dea27fa
Brad Rice <bradrice1@gmail.com>
2019-05-24 22:45:48 -0400

    added carousel to home page

--- home/models.py
+++ home/models.py
@@ -1,10 +1,29 @@
 from django.db import models
-
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, MultiFieldPanel, InlinePanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
+class HomePageCarouselImages(Orderable):
+    """Between 1 and 5 images for the home page carousel."""
+
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    caption = models.CharField(max_length=100, blank=False, null=True)
+
+    panels = [
+        MultiFieldPanel([
+            ImageChooserPanel("carousel_image"),
+        FieldPanel("caption")
+        ])
+        ]
 
 class HomePage(Page):
     """Home page model."""
@@ -32,11 +51,20 @@ class HomePage(Page):
 
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title"),
-        FieldPanel("banner_subtitle"),
-        ImageChooserPanel("banner_image"),
-        PageChooserPanel("banner_cta"),
-        FieldPanel("feature_story")
+         MultiFieldPanel(
+            [
+                FieldPanel("banner_title"),
+                FieldPanel("banner_subtitle"),
+                ImageChooserPanel("banner_image"),
+                PageChooserPanel("banner_cta"),
+            ],
+            heading="Banner Options",
+        ),
+        FieldPanel("feature_story"),
+        MultiFieldPanel(
+            [InlinePanel("carousel_images", max_num=5, min_num=1, label="Image")],
+            heading="Carousel Images",
+        )
     ]
 
     class Meta:
