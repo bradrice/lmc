11e1419200cd08cd1f4097a50af2780cb9186792
Brad Rice <bradrice1@gmail.com>
2019-05-29 12:08:44 -0400

    added blog

---
+++ blog/models.py
@@ -0,0 +1,83 @@
+"""Blog listing and blog detail pages."""
+from django.db import models
+
+from datetime import date
+
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField, RichTextField
+from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+from streams import blocks
+
+
+class BlogListingPage(Page):
+    """Listing page lists all the Blog Detail Pages."""
+
+    template = "blog/blog_listing_page.html"
+    max_count = 1
+    subpage_types = ['BlogDetailPage']
+
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text='Overwrites the default title',
+    )
+    description = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+        FieldPanel("description")
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        """Adding custom stuff to our context."""
+        context = super().get_context(request, *args, **kwargs)
+        context["posts"] = BlogDetailPage.objects.live().public()
+        return context
+
+
+class BlogDetailPage(Page):
+    """Blog detail page."""
+
+    date_composed = models.DateField(
+        default=date.today,
+        verbose_name="Composition Date"
+    )
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text='Overwrites the default title',
+    )
+    summary = RichTextField(
+        blank=True, 
+        null=True,
+        help_text="Teaser text will show on the blog listing page"
+        )
+    blog_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichtextBlock()),
+            ("simple_richtext", blocks.SimpleRichtextBlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("date_composed"),
+        FieldPanel("custom_title"),
+        FieldPanel("summary"),
+        ImageChooserPanel("blog_image"),
+        StreamFieldPanel("content"),
+    ]
